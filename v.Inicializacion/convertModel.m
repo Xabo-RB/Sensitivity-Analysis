9ab20c8a6f86f108d38b5d8148e3862b04d1350f
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-06-06 17:37:35 +0200

    s

--- v.Inicializacion/convertModel.m
+++ v.Inicializacion/convertModel.m
@@ -7,6 +7,48 @@ symbols = load(modelName, 'x','f','p','u','w');
 
 newEQNS = convertEQNs(symbols)
 
+% Crear archivo para almacenar la ode
+[~, nombreFuncion, ~] = fileparts(modelName);
+fid = fopen(nombreFuncion + ".m", "w");
+if fid == -1
+    error("Prarece que no funciona");
+end
 
+if isempty(symbols.u) && isempty(symbols.w)
+    fprintf(fid, "function dx = %s(t, x, p)\n", nombreFuncion);
+    fprintf(fid, "    dx = zeros(%d,1);\n", numel(newEQNS));
+    fprintf(fid, "\n");
+
+    for i = 1:numel(newEQNS)
+        ecuacion = char(newEQNS(i));
+        fprintf(fid, "    dx(%d) = %s;\n", i, ecuacion);
+    end
+    fprintf(fid, "end\n");
+else
+    
+    if ~isempty(symbols.u) && isempty(symbols.w)
+        ustring = string(symbols.u);
+        oracionUs = strjoin(ustring, ", ");
+        oracionUs = char(oracionUs);
+    elseif ~isempty(symbols.w) && isempty(symbols.u)
+        ustring = string(symbols.w);
+        oracionUs = strjoin(ustring, ", ");
+        oracionUs = char(oracionUs);
+    end
+
+    fprintf(fid, "function dx = %s(t, x, p, %s)\n", nombreFuncion, oracionUs);
+    fprintf(fid, "    dx = zeros(%d,1);\n", numel(newEQNS));
+    fprintf(fid, "\n");
+    
+    for i = 1:numel(newEQNS)
+        ecuacion = char(newEQNS(i));
+        fprintf(fid, "    dx(%d) = %s;\n", i, ecuacion);
+    end
+
+    fprintf(fid, "end\n");
+
+end
+
+fclose(fid);
 
 
