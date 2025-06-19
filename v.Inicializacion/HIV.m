627ded74dc40ff972938f13e80258d4aca51585e
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-06-19 20:38:09 +0200

    a

--- v.Inicializacion/HIV.m
+++
@@ -1,7 +0,0 @@
-function dx = HIV(t, x, p, u1)
-    dx = zeros(3,1);
-
-    dx(1) = p(1) - x(1)*p(2) - x(1)*x(3)*u1;
-    dx(2) = x(1)*x(3)*u1 - x(2)*p(4);
-    dx(3) = p(3)*x(2)*p(4) - x(3)*p(5);
-end
