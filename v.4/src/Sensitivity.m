9a7c83a56c6c1ccf36e6a3aa19fd537a1f2c9842
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-08-01 11:10:09 +0200

    Comentar código

--- v.4/src/Sensitivity.m
+++ v.4/src/Sensitivity.m
@@ -1,4 +1,6 @@
 % === SENSITIVITY CODE ===
+% This script evaluates the sensitivity of an ODE model with respect to an user-defined set of parameters.
+% For each parameter, it generates a normalized sensitivity matrix and produces a results figure.
 
 % Create the name of the function handle type variable that is going to analyse
 ode_function  = str2func(modelname);
@@ -11,6 +13,7 @@ else
     error(['File ' options_script '.m not found']);
 end
 
+% Initialize the parallel processing pool if it is not active
 if isempty(gcp('nocreate'))
     parpool;
 end
@@ -20,17 +23,22 @@ x0_real = real(x0);
 num_params = length(param_values);
 
 % === LOOP OF PARAMETERS TO ANALYZE ===
-for pI = 1:num_params  %%%%% changeee
+for pI = 1:num_params  
     tic
     rango_max_param = 5 * param_values_original(pI);
     ModelVect = linspace(0, rango_max_param, t_end);
     results_matrix = zeros(t_end, length(tspan));
 
     parfor i = 1:t_end
+
         local_params = param_values_original;
         local_params(pI) = ModelVect(i);
-        p = complex(local_params, 0);
+        p = complex(local_params, 0);        
+
+        % Run the ODE integration with the modified parameters
         sol = sensitivityMain(x0_real, p, 1e-16, tspan, ode_function, solver, rel_tol, abs_tol);
+
+        % Extract the response corresponding to the status of interest
         response = sol{state_index}(:, pI + 1);
         normalized = (response .* ModelVect(i)) ./ sol{state_index}(:, 1);
         results_matrix(i, :) = normalized.';
